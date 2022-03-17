43bb3ab6f47bdac4ff1ce09b1ec159e86ae905b3
snowingfox <1503401882@qq.com>
2022-03-17 21:55:48 +0800

    :fire: chore: format code

--- src/_mock_/index.ts
+++ src/_mock_/index.ts
@@ -1,4 +1,5 @@
 import type { NotificationsDto } from '@/shared/Dto/notifications.dto'
+
 export const avatarUrl = 'http://p3.music.126.net/F3sugpmX0WXG0lZkqVkG2g==/109951166049791621.jpg?param=200y200'
 
 export const NotifyListData: NotificationsDto[] = [
