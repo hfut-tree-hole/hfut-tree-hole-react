4456799d60ebb41325398e43e2f53ad8d7fdb8f4
snowingfox <1503401882@qq.com>
2022-03-23 21:00:37 +0800

    :fire: chore: change props

--- src/components/base/BaseIcon/BaseIcon.tsx
+++ src/components/base/BaseIcon/BaseIcon.tsx
@@ -1,9 +1,10 @@
 import { Icon } from '@iconify/react'
 import type { SxProps } from '@mui/material'
 import { Box } from '@mui/material'
+import type { BoxProps } from '@mui/material/Box/Box'
 
-export function BaseIcon({ icon, sx }: { icon: string; sx: SxProps }) {
+export function BaseIcon({ icon, sx, ...boxProps }: { icon: string; sx: SxProps; boxProps?: BoxProps }) {
   return <>
-    <Box component={Icon} icon={icon} sx={sx}/>
+    <Box component={Icon} icon={icon} sx={sx} {...(boxProps || {})}/>
   </>
 }
