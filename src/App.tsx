bd4a31818989e2725d0ecaeb3d433090167305aa
snowingfox <1503401882@qq.com>
2022-03-23 22:32:02 +0800

    :zap: chore: delete useless import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { RootRoutes } from '@/router'
 import './App.css'
+import '@fullcalendar/common/main.css'
+import '@fullcalendar/daygrid/main.css'
+import '@fullcalendar/timegrid/main.css'
 import { AppLayout } from '@/layouts/AppLayout/AppLayout'
 
 function App() {
