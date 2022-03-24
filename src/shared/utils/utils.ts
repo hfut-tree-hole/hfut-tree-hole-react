0aab0868a7ce1beb4ab1f8a888a3c260abe1411f
snowingfox <1503401882@qq.com>
2022-03-24 22:31:07 +0800

    :zap: feat: transform time

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,3 +1,7 @@
-export function transformTime(time: number) {
+export function transformBeforeTime(time: number) {
   return Math.ceil((Date.now() - time) / 1000 / 60)
+}
+
+export function transformCalendarTime(time: Date) {
+  return `${time.getFullYear()}年 ${time.getMonth()}月${time.getDate()}日`
 }
