6b6b1ef1959a0774f55ab580401e14f3b812d7d2
snowingfox <1503401882@qq.com>
2022-03-24 22:29:14 +0800

    :art: feat: change layout

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -2,20 +2,27 @@ import Box from '@mui/material/Box'
 import type { ReactNode } from 'react'
 import { styled } from '@mui/material/styles'
 import type { BoxProps } from '@mui/material/Box/Box'
+import { AppLayoutConfig } from '../../shared/config/layout'
 import { Footer } from '@/components/Footer'
 import { useDrawer } from '@/layouts/AppLayout/use-drawer'
 import { AppHeader } from '@/layouts/AppLayout/Header/Header'
 import { Drawer, drawerWidth } from '@/layouts/AppLayout/Drawer/Drawer'
 
 const RouterView = styled(Box)<BoxProps>(({ theme }) => ({
+  maxWidth: '100vw',
   position: 'relative',
+  paddingLeft: 16,
+  paddingRight: 16,
+  marginLeft: AppLayoutConfig.DRAWER.DASHBOARD_COLLAPSE_WIDTH,
+  paddingTop: AppLayoutConfig.HEADER.DASHBOARD_DESKTOP_HEIGHT + 24,
+  paddingBottom: AppLayoutConfig.HEADER.DASHBOARD_DESKTOP_HEIGHT + 24,
   [theme.breakpoints.up('sm')]: {
-    left: `${drawerWidth}px`,
-    width: `calc(100vw - ${drawerWidth})px`,
+    marginLeft: `${drawerWidth}px`,
+    maxWidth: `calc(100vw - ${drawerWidth})px`,
+    paddingTop: AppLayoutConfig.HEADER.MOBILE_HEIGHT + 24,
+    paddingBottom: AppLayoutConfig.HEADER.MOBILE_HEIGHT + 24,
   },
-  left: 0,
-  top: 60,
-  width: '100vw',
+  overflow: 'hidden',
 }))
 
 export function AppLayout(props: { children: ReactNode }) {
@@ -23,7 +30,7 @@ export function AppLayout(props: { children: ReactNode }) {
 
   return (
     <>
-      <Box className={'relative w-screen h-screen'}>
+      <Box className={'relative w-full h-screen overflow-hideen'}>
         <AppHeader open={open} handleDrawerOpen={handleDrawerOpen} />
         <Drawer
           variant={variant}
