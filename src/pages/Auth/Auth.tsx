d7fee3adc7924e2919a0040f1e17aed190e95eb6
snowingfox <1503401882@qq.com>
2022-04-02 22:08:08 +0800

    :art: feat: layout

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,10 +1,19 @@
 import { Box } from '@mui/material'
 import { Outlet } from 'react-router-dom'
+import bgImg from './bg.jpg'
 
 export function Auth() {
   return <>
-    <Box className={'width-full height-screen flex justify-center items-center'}>
-      <Outlet />
+    <Box className={'w-screen h-screen flex justify-center items-center overflow-hidden'}>
+      <Box className={'absolute w-full h-full'}>
+        <img src={bgImg} className={'absolute w-full h-full object-cover'}/>
+        <Box className={'absolute w-full h-full z-1'} sx={{
+          backgroundColor: 'rgba(0,0,0,.4)',
+        }}/>
+      </Box>
+      <Box className={'absolute z-3 bg-white p-5 rounded-lg shadow-lg shadow-slate-700'}>
+        <Outlet />
+      </Box>
     </Box>
   </>
 }
