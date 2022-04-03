dbf350956657ff1ca1ce7fdcbe61962495301792
snowingfox <1503401882@qq.com>
2022-04-03 15:40:37 +0800

    :fire: feat: better logic

--- src/components/base/Logo/Logo.tsx
+++ src/components/base/Logo/Logo.tsx
@@ -1,8 +1,9 @@
 import logo from '@public/img/logo.png'
 import { Box } from '@mui/material'
+import { mountOptionalClsName } from '@/shared/utils/dom'
 
 export function Logo(props: { className?: string }) {
   return <>
-    <Box component={'img'} src={logo} className={`${props.className ? props.className : 'w-12 h-12'} rounded-full`}/>
+    <Box component={'img'} src={logo} className={`w-12 h-12 rounded-full ${mountOptionalClsName(props.className)} `}/>
   </>
 }
---
+++ src/shared/utils/dom.ts
@@ -0,0 +1,3 @@
+export function mountOptionalClsName(clsName?: string, defaultVal = '') {
+  return clsName || defaultVal
+}
