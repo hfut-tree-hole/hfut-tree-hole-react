0732638ba4b509680b6c3d6a6d16cc5a1989f742
snowingfox <1503401882@qq.com>
2022-04-03 15:36:26 +0800

    :art: feat: add logo comp

--- src/components/base/Logo/Logo.tsx
+++ src/components/base/Logo/Logo.tsx
@@ -1,1 +1,8 @@
-export function Logo() {}
+import logo from '@public/img/logo.png'
+import { Box } from '@mui/material'
+
+export function Logo(props: { className?: string }) {
+  return <>
+    <Box component={'img'} src={logo} className={`${props.className ? props.className : 'w-12 h-12'} rounded-full`}/>
+  </>
+}
